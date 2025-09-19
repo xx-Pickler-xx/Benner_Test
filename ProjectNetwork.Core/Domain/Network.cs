b6f0f68e7f285f848abb12f72ed5f656371d73a2
Guilherme Pickler PC <pickler.008@outlook.com>
2025-09-19 16:47:09 -0300

    Construtor básico com inicialização de elements

--- ProjectNetwork.Core/Domain/Network.cs
+++ ProjectNetwork.Core/Domain/Network.cs
@@ -4,6 +4,19 @@ namespace ProjectNetwork.Core.Domain
 {
     public class Network
     {
-       
+        private int[] _elements;
+
+        public Network(int number_of_elements)
+        {
+            if (number_of_elements <= 0)
+                throw new ArgumentException("O número de elementos deve ser positivo.");
+
+            _elements = new int[number_of_elements];
+
+            for (int i = 0; i < number_of_elements; i++)
+            {
+                _elements[i] = i;
+            }
+        }
     }
 }
